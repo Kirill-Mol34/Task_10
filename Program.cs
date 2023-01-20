8a7de11050952564000afc8984d0852c17f4ac30
Kirill <kir.molokoff@yandex.ru>
2023-01-20 21:03:57 +0900

    fin

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿int num1 = 456;
+int num2 = 782;
+int num3 = 918;
+System.Console.WriteLine($"{num1} -> {num1 / 10 % 10} ");
+System.Console.WriteLine($"{num2} -> {num2 / 10 % 10} ");
+System.Console.WriteLine($"{num3} -> {num3 / 10 % 10} ");
+System.Console.WriteLine();
